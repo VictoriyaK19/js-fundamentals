f31c591294f084eb3e43ff47549a987f06f9e4e7
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-27 18:45:58 +0300

    commit

--- 01-basic-syntax/theatre_promotions.js
+++ 01-basic-syntax/theatre_promotions.js
@@ -1,1 +1,48 @@
-function
+function teatrePromotions(day, age) {
+    if (age < 0 || age > 122) {
+        console.log("Error!");
+    } else if (age <= 18) {
+        
+        switch(day){
+            case 'Weekday':
+                console.log('12$');
+                break;
+            case 'Weekend':
+                console.log('15$');
+                break;
+            case 'Holiday':
+                console.log('5$');
+                break;
+        }
+    } else if (age <= 64) {
+
+        switch(day){
+            case 'Weekday':
+                console.log('18$');
+                break;
+            case 'Weekend':
+                console.log('20$');
+                break;
+            case 'Holiday':
+                console.log('12$');
+                break;
+        }
+    } else if (age <= 122) {
+        
+        switch(day){
+            case 'Weekday':
+                console.log('12$');
+                break;
+            case 'Weekend':
+                console.log('15$');
+                break;
+            case 'Holiday':
+                console.log('10$');
+                break;
+        }
+    }
+}
+
+teatrePromotions('Weekday', 42);
+teatrePromotions('Holiday', -12);
+teatrePromotions('Holiday', 15);

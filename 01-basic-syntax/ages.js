d4516b3733133babb2ad64de4995664f14bbe856
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-29 10:29:47 +0300

    commit

---
+++ 01-basic-syntax/ages.js
@@ -0,0 +1,21 @@
+function ageReveal(age) {
+    if (age < 0) {
+        console.log("out of bounds");
+    }  else if (age <= 2) {
+        console.log("baby");
+    } else if (age <= 13) {
+        console.log("child");
+    } else if (age <= 19) {
+        console.log("teenager");
+    } else if (age <= 65) {
+        console.log("adult");
+    } else if (age >= 66) {
+        console.log("elder");
+    }
+    
+}
+
+ageReveal(20);
+ageReveal(1);
+ageReveal(100);
+ageReveal(-1);

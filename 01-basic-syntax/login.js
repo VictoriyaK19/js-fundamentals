7ddf7ba0c28d0a4d673e2955f7706202b8f157e4
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-30 11:08:31 +0200

    commit

---
+++ 01-basic-syntax/login.js
@@ -0,0 +1,25 @@
+function login(array) {
+
+    let uncorrectPasswords = 0;
+
+    const username = array.shift().toString();
+    const password = username.split('').reverse().join('');
+
+    array.forEach(element => {
+        if (element == password) {
+            console.log(`User ${username} logged in.`);
+        } else {
+            uncorrectPasswords++;
+            if (uncorrectPasswords == 4) {
+                console.log(`User ${username} blocked!`)
+            } else {
+                console.log('Incorrect password. Try again.');
+            }
+   
+        } 
+    });
+}
+
+login(['Acer','login','go','let me in','recA']);
+login(['momo','omom']);
+login(['sunny','rainy','cloudy','sunny','not sunny']);

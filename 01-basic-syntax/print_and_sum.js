4006668d2bea87a18a3c1784f80cec675625ba8b
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-30 09:33:15 +0200

    commit

---
+++ 01-basic-syntax/print_and_sum.js
@@ -0,0 +1,15 @@
+function printSum(n1, n2) {
+    let sum = 0;
+    var numbers = '';
+
+    for (var num = n1; num <= n2; num++) {
+        numbers += num + ' ';
+        sum += num;
+    }
+    console.log(numbers);
+    console.log(`Sum: ${sum}`);
+}
+
+printSum(5, 10);
+printSum(0, 26);
+printSum(50, 60);

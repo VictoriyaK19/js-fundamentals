57617f975b181797dc6579831c878c818e7c9775
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-01 10:22:12 +0200

    commit

---
+++ 01-basic-syntax/distance_between_points.js
@@ -0,0 +1,16 @@
+function calculateDistance(x1, y1, x2, y2) {
+    // we will use pythagorean theorem
+    // a^2 + b^2 = c^2
+    // a & b are both sides of 90 deg triangle
+    // c = Math.Sqrt(x^2 + b^2)
+    // is the hypotenuse
+    let a = Math.abs(x1 - x2);
+    let b = Math.abs(y1 - y2);
+
+    // distance is "c"
+    let distance = Math.sqrt((a * a) + (b * b));
+    console.log(distance);
+}
+
+calculateDistance(2, 4, 5, 0);
+calculateDistance(2.34, 15.66, -13.55, -2.9985);

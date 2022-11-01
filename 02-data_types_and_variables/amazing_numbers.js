5b513357b4194e757ba734777c2a93ea3548f3e3
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-01 12:17:26 +0200

    commit

---
+++ 02-data_types_and_variables/amazing_numbers.js
@@ -0,0 +1,18 @@
+function amazingOrNot(num) {
+    num = num.toString();
+    let sum = 0;
+
+    for (let index = 0; index < num.length; index++) {
+        sum += Number(num[index]);
+    }
+    // let result = sum.toString().includes('9');
+    // console.log(result ? `${num} Amazing? True`: 
+    //                     `${num} Amazing? False`);
+    
+    let result = sum.toString();
+    console.log(result.includes('9') ?  `${num} Amazing? True`: 
+                                        `${num} Amazing? False`);
+}
+
+amazingOrNot(1233);
+amazingOrNot(999);

6a4886604bf680d329e43248ef723a993d1552c3
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-01 15:43:09 +0200

    commit

---
+++ 02-data_types_and_variables/required_reading.js
@@ -0,0 +1,14 @@
+function solve(pages, pgReadInHour, days) {
+    let hoursPerDay = pages / pgReadInHour / days;
+    console.log(hoursPerDay);
+}
+
+solve(212,
+    20 ,
+    2 
+    );
+solve(432,
+    15 ,
+    4 
+    );
+

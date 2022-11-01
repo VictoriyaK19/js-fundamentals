80c3b1042f45fef7a8708f9cefbee5db03eb48be
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-01 10:55:16 +0200

    commit

---
+++ 02-data_types_and_variables/concatenate_names.js
@@ -0,0 +1,13 @@
+function concatenate(strind1, string2, delimiter) {
+    console.log(`${strind1}${delimiter}${string2}`);
+}
+concatenate('John', 'Smith', '->'
+);
+concatenate('Jan',
+'White',
+'<->'
+);
+concatenate('Linda',
+'Terry',
+'=>'
+);

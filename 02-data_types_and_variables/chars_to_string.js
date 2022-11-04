73582ee1faac9f9155e33339b14486e67de7e335
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-04 18:05:18 +0200

    commit

---
+++ 02-data_types_and_variables/chars_to_string.js
@@ -0,0 +1,17 @@
+function charsToString(ch1, ch2, ch3) {
+    let array = [ch1, ch2, ch3];
+    console.log(array.join(''));
+}
+
+charsToString('a',
+'b',
+'c'
+);
+charsToString('%',
+'2',
+'o'
+);
+charsToString('1',
+'5',
+'p'
+);

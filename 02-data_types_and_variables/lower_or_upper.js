0642ff679a0fabb984d99b96e3f52e25d1aecd78
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-11-05 23:01:25 +0200

    commit

---
+++ 02-data_types_and_variables/lower_or_upper.js
@@ -0,0 +1,10 @@
+function solve(letter) {
+    console.log(letter == letter.toUpperCase() 
+                    ? 'upper-case': 'lower-case');
+       
+}
+
+solve('L');
+solve('f');
+
+
---
+++ 02-data_types_and_variables/reversed_chars.js
@@ -0,0 +1,10 @@
+function reversed(ch1, ch2, ch3) {
+
+    console.log(`${ch3} ${ch2} ${ch1}`);
+}
+
+reversed('A', 'B', 'C');
+reversed('1',
+'L',
+'&'
+);
